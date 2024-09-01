a4680142f8a24107fe7da687bf3d176510753c6a
Rahul Dey <rahuldey832000@gmail.com>
2024-09-01 21:11:43 +0530

    Design Flyout Section

--- IcecreamMAUI/AppShell.xaml.cs
+++ IcecreamMAUI/AppShell.xaml.cs
@@ -30,4 +30,15 @@ public partial class AppShell : Shell
         //Routing.RegisterRoute(nameof(SigninPage), typeof(SigninPage));
         //Routing.RegisterRoute(nameof(SignupPage), typeof(SignupPage));
     }
+
+    private async void GoToGithubPage(object sender, TappedEventArgs e)
+    {
+        await Launcher.OpenAsync("https://www.github.com/rahul8320");
+    }
+
+    private async void Signout(object sender, EventArgs e)
+    {
+        await Current.DisplayAlert("Alert", "You will be signout", "Ok");
+    }
+
 }
