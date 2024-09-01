ef9bf2ed6ba4857d83d7948d79847e6033ae1e44
Rahul Dey <rahuldey832000@gmail.com>
2024-09-01 23:03:02 +0530

    Update Status Bar Color

--- IcecreamMAUI/MauiProgram.cs
+++ IcecreamMAUI/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace IcecreamMAUI
 {
@@ -13,10 +14,11 @@ namespace IcecreamMAUI
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                })
+                .UseMauiCommunityToolkit();
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
