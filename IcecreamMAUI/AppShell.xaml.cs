6e56369eb1fcccc1b022e6b4489d9a4619978f49
Rahul Dey <rahuldey832000@gmail.com>
2024-09-01 20:15:36 +0530

    Add Signin and Signup Page routes

--- IcecreamMAUI/AppShell.xaml.cs
+++ IcecreamMAUI/AppShell.xaml.cs
@@ -1,10 +1,33 @@
-﻿namespace IcecreamMAUI
+﻿using IcecreamMAUI.Pages;
+
+namespace IcecreamMAUI;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    private readonly static Type[] _routablePageType =
+       [
+           typeof(SigninPage),
+            typeof(SignupPage),
+            typeof(DetailsPage),
+            typeof(MyOrderPage),
+            typeof(OrderDetailsPage)
+       ];
+
+    public AppShell()
     {
-        public AppShell()
+        InitializeComponent();
+
+        RegisterRoutes();
+    }
+
+    private static void RegisterRoutes()
+    {
+        foreach (var pageType in _routablePageType)
         {
-            InitializeComponent();
+            Routing.RegisterRoute(pageType.Name, pageType);
         }
+
+        //Routing.RegisterRoute(nameof(SigninPage), typeof(SigninPage));
+        //Routing.RegisterRoute(nameof(SignupPage), typeof(SignupPage));
     }
 }
