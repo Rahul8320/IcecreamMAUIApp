3d33a1a20012b8af7620953ce1b32399d011b69c
Rahul Dey <rahuldey832000@gmail.com>
2024-09-03 21:05:55 +0530

    Update service result

--- IcecreamMAUI.Shared/Models/ServiceResult.cs
+++ IcecreamMAUI.Shared/Models/ServiceResult.cs
@@ -1,13 +1,38 @@
+using System.Net;
+
 namespace IcecreamMAUI.Shared.Models;
 
-public record ServiceResult(bool IsSuccess, string? ErrorMessage)
+public record ServiceResult(HttpStatusCode StatusCode, string? ErrorMessage, List<string>? ErrorMessages)
 {
-    public static ServiceResult Success() => new(true, null);
-    public static ServiceResult Failure(string? errorMessage) => new(false, errorMessage);
+    public static ServiceResult Success()
+        => new(HttpStatusCode.OK, null, null);
+    public static ServiceResult BadRequest(string? errorMessage, List<string>? errorMessages = null)
+        => new(HttpStatusCode.BadRequest, errorMessage, errorMessages);
+
+    public static ServiceResult NotFound(string errorMessage)
+        => new(HttpStatusCode.NotFound, errorMessage, null);
+
+    public static ServiceResult ServerError(string errorMessage)
+        => new(HttpStatusCode.InternalServerError, errorMessage, null);
+
+    public static ServiceResult NoContent()
+        => new(HttpStatusCode.NoContent, null, null);
 }
 
-public record ServiceResult<T>(bool IsSuccess, T Data, string? ErrorMessage) : ServiceResult(IsSuccess, ErrorMessage) where T : class
+public record ServiceResult<T>(HttpStatusCode StatusCode, T? Data, string? ErrorMessage, List<string>? ErrorMessages)
 {
-    public static ServiceResult<T> Success(T data) => new(true, data, null);
-    public static new ServiceResult<T> Failure(string? errorMessage) => new(false, default!, errorMessage);
+    public static ServiceResult<T> Success(T data)
+        => new(HttpStatusCode.OK, data, null, null);
+
+    public static ServiceResult<T> BadRequest(string? errorMessage, List<string>? errorMessages = null)
+        => new(HttpStatusCode.BadRequest, default, errorMessage, errorMessages);
+
+    public static ServiceResult<T> NotFound(string errorMessage)
+        => new(HttpStatusCode.NotFound, default, errorMessage, null);
+
+    public static ServiceResult<T> ServerError(string errorMessage)
+        => new(HttpStatusCode.InternalServerError, default, errorMessage, null);
+
+    public static ServiceResult<T> NoContent()
+        => new(HttpStatusCode.NoContent, default, null, null);
 }
